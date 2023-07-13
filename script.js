9826b3044f9989040419dcf349369f3171ac0145
kingstonskye <125080702+kingstonskye@users.noreply.github.com>
2023-07-13 15:25:09 +0900

    Update script.js

--- script.js
+++ script.js
@@ -19,16 +19,34 @@ function countUp1() {
   if (playerOneScore === 100) {
     return
   }
+  
+  if (playerTwoScore === 100) {
+    return
+  }
+  
   playerOneScore += 1;
   playerOneCount.textContent = playerOneScore;
+  
+  if (playerOneScore === 100) {
+    displayWinnerOne();
+  }
 };
 
 function countUp2() {
   if (playerTwoScore === 100) {
     return
   }
+  
+  if (playerOneScore === 100) {
+    return
+  }
+  
   playerTwoScore += 1;
   playerTwoCount.textContent = playerTwoScore;
+  
+  if (playerTwoScore === 100) {
+    displayWinnerTwo();
+  }
 };
 
 document.addEventListener("keydown", function
@@ -37,10 +55,49 @@ document.addEventListener("keydown", function
         countUp2();
 })
 
+//RESET BUTTON
+
 function deleteScore() {
   playerOneScore = 0;
   playerTwoScore = 0;
   playerOneCount.textContent = playerOneScore;
   playerTwoCount.textContent = playerTwoScore;
+  var winnerTwoText = document.getElementById("winner2");
+  var winnerOneText = document.getElementById("winner1");
+  winnerTwoText.style.display = "none"; //deletes winner upon reset
+  winnerOneText.style.display = "none"; //deletes winner upon reset
 }
 
+//WINNER ANNOUNCEMENT
+
+function displayWinnerOne() {
+  var winnerOneText = document.getElementById("winner1");
+  winnerOneText.style.display = "block";
+}
+
+function displayWinnerTwo() {
+  var winnerTwoText = document.getElementById("winner2");
+  winnerTwoText.style.display = "block";
+}
+
+//TIMER
+
+let timeRemaining = 3
+let timerElement =document.getElementById("Countdown")
+
+function countdown() {
+  timeRemaining = timeRemaining -1;
+  if(timeRemaining <= 0) {
+  timerElement.innerText = "GO GO GO!" 
+  }
+  else {
+    timerElement.innerText = timeRemaining
+  console.log(timeRemaining)
+}
+}
+
+function Countdown () {
+  console.log(20)
+}
+
+setInterval(countdown, 1000)
